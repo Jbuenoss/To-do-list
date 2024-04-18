86778b22314bd3b7f5752cedf13685624555959b
Joao Bueno <joaobueno121212@gmail.com>
2024-04-18 20:10:19 -0300

    Completed save tasks in the browser but not yet the state

--- js/control.js
+++ js/control.js
@@ -1,41 +1,56 @@
-let contador = 0;
 const input = document.getElementById("inputTask");
 const btnAdd = document.getElementById("btnAdd");
 const main = document.getElementById("listArea");
+
+let contador = [];
+for(let i = 1; i < 100; i++){
+    contador.push(i);
+}
+// let contador = 0;
+let indice = 0;
 
 function addTask() {
     let text = input.value;
 
     if ((text !== "") && (text !== null) && (text !== undefined)) {
-        ++contador;
+        
+        while(indice <= 100 && contador[indice] == undefined){
+            indice++;
+        }
 
-        //acute accent
-        let newItem =
-        `<div id=${contador} class="item">
-            <div onclick="markTask(${contador})" class="itemIcon">
-                <span id=icon_${contador} class="material-symbols-outlined">radio_button_unchecked</span>
-            </div>
-            <div onclick="markTask(${contador})" class="itemName">
-                ${text}
-            </div>
+        if(contador[indice] != undefined){
+            //acute accent
+            let newItem = 
+            `<div id=${contador[indice]} class="item">
+                <div onclick="markTask(${contador[indice]})" class="itemIcon">
+                    <span id=icon_${contador[indice]} class="material-symbols-outlined">radio_button_unchecked</span>
+                </div>
+                <div onclick="markTask(${contador[indice]})" class="itemName">
+                    ${text}
+                </div>
 
-            <div class="toUp">
-                <button onclick="moveToUp(${contador})"><span class="material-symbols-outlined">expand_less</span></button>
-            </div>
-            <div class="toDown">
-                <button onclick="moveToDown(${contador})"><span class="material-symbols-outlined">expand_more</span></button>
-            </div>
+                <div class="toUp">
+                    <button onclick="moveToUp(${contador[indice]})"><span class="material-symbols-outlined">expand_less</span></button>
+                </div>
+                <div class="toDown">
+                    <button onclick="moveToDown(${contador[indice]})"><span class="material-symbols-outlined">expand_more</span></button>
+                </div>
 
-            <div class="itemButton">
-                <button onclick="deleteTask(${contador})" class="delete"><span class="material-symbols-outlined">delete</span>Delete</button>
-            </div>
-        </div>`
+                <div class="itemButton">
+                    <button onclick="deleteTask(${contador[indice]})" class="delete"><span class="material-symbols-outlined">delete</span>Delete</button>
+                </div>
+            </div>`
 
-        main.innerHTML += newItem;
+            main.innerHTML += newItem;
+
+            saveTasks(newItem);
+
+            indice++;
+        }
+    
         input.value = "";
         //to add a new thing
         input.focus();
-
     }
 }
 
@@ -85,3 +100,41 @@ function markTask(id) {
         icon.textContent = "radio_button_unchecked";
     }
 }
+
+function saveTasks(item){
+    let previous = JSON.parse(localStorage.getItem("tasks")) || [];
+    // var previous = JSON.parse(localStorage.getItem("tasks"));
+    previous.push(item);
+
+    let current = JSON.stringify(previous);
+    localStorage.setItem("tasks", current);
+}
+
+function loadTasks(){
+    let current = JSON.parse(localStorage.getItem("tasks"));
+
+    const listAreaElement = document.getElementById("listArea");
+    
+    if(current){
+        for(item of current){
+            listAreaElement.innerHTML += item;
+        }
+    }
+}
+
+function updateId(){
+    const tasks = [...document.getElementsByClassName("item")];
+
+    for(task of tasks){
+        console.log(task.id-1);
+        delete contador[task.id-1];
+    }
+}
+
+window.onload = () => {
+    // localStorage.clear();
+    loadTasks();
+
+    //update id element
+    updateId();
+};
